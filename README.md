ccea9660bcfa5f822d51bf520641338e1a60e52f
Dawson McCall <dawsonmccall62@gmail.com>
2026-02-02 22:08:46 +0000

    base README update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Simulated Self Driving Car Using a Neural Network
+
+## Tech Stack
+- **Vanilla JavaScript** — All program logic including neural network
+- **CSS**
+- **HTML**
+
+## Links
+**Repository:** [neural-network-self-driving-car](https://github.com/grey-otoc/neural-network-self-driving-car)
--- src/main.js
+++ src/main.js
@@ -31,8 +31,8 @@ const traffic = [
     new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
     new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
     new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(1), -900, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(0), -800, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -1100, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -1000, 30, 50, "DUMMY", 2),
     new Car(road.getLaneCenter(2), -1200, 30, 50, "DUMMY", 2)
 ];
 
