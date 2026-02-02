296d8a864b7f706b0797e1555e6141124ae6c306
Dawson McCall <dawsonmccall62@gmail.com>
2026-02-02 18:21:34 +0000

    finalised sensors and sensor detection methodology, added logic for traffic, and began neural network

--- src/sensors.js
+++ src/sensors.js
@@ -9,20 +9,24 @@ class Sensor {
         this.readings = [];
     }
 
-    update(roadBorders) {
+    update(roadBorders, traffic) {
         this.#castRays();
         this.readings = [];
         for(let i = 0; i < this.rays.length; i++) {
             this.readings.push(
-                this.#getReading(this.rays[i], roadBorders)
+                this.#getReading(
+                    this.rays[i],
+                    roadBorders,
+                    traffic
+                )
             );
         }
     }
 
-    #getReading(ray, roadBorders) {
+    #getReading(ray, roadBorders, traffic) {
         let touches = [];
 
-        for(let i = 0; i < roadBorders.length; i++) {
+        for (let i = 0; i < roadBorders.length; i++) {
             const touch = getIntersection(
                 ray[0],
                 ray[1],
@@ -30,14 +34,32 @@ class Sensor {
                 roadBorders[i][1]
             );
 
-            if(touch) {
+            if (touch) {
                 touches.push(touch);
+            }
+        }
+
+        for (let i = 0; i < traffic.length; i++) {
+            const poly = traffic[i].polygon;
+            for (let j = 0; j < poly.length; j++) {
+                const value = getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j + 1) % poly.length]
+                );
+
+                if (value) {
+                    touches.push(value);
+                }
             }
         }
 
         if (touches.length == 0) {
             return null;
         } else {
+            // out of all the objects sensors are currently touching, select the
+            // one closest to the car
             const offsets = touches.map(e => e.offset);
             const minOffset = Math.min(...offsets);
             return touches.find(e => e.offset == minOffset);
@@ -68,6 +90,11 @@ class Sensor {
 
     draw(ctx) {
         for (let i = 0; i < this.rayCount; i++) {
+            let end = this.rays[i][1];
+            if (this.readings[i]) {
+                end = this.readings[i];
+            }
+
             ctx.beginPath();
             ctx.lineWidth = 2;
             ctx.strokeStyle = "yellow";
@@ -76,8 +103,22 @@ class Sensor {
                 this.rays[i][0].y
             );
             ctx.lineTo(
-                this.rays[i][1].x,
+                end.x,
+                end.y
+            );
+            ctx.stroke();
+
+            // show the portion of the sensor line that extends beyond the collision
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "black";
+            ctx.moveTo(
+                this.rays[i][1].x, 
                 this.rays[i][1].y
+            );
+            ctx.lineTo(
+                end.x,
+                end.y
             );
             ctx.stroke();
         }
